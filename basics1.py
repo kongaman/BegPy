7ddf295eba2110c096c3c1ec160415c61157adb4
Chris <christian.kranz@gmail.com>
2019-08-14 20:43:27 +0200

    Nummernraten

--- basics1.py
+++ basics1.py
@@ -1,3 +1,18 @@
-name = 'Mary'
-if name == 'Mary':
-    print('Hello Dear')
+import random
+secretnumber = random.randint(1,20)
+print('Ich denke an eine Zahl zwischen 1 und 20.')
+for guessesTaken in range(1,7):
+    print('Rate mal!')
+    guess = int(input())
+
+    if guess < secretnumber:
+        print('Zu niedrig!')
+    elif guess > secretnumber:
+        print('Zu hoch!')
+    else:
+        break
+
+if guess == secretnumber:
+    print('Richtig , du hast es in ' + str(guessesTaken) + ' Versuchen erraten.')
+else:
+    print('Nö, die Zahl an die ich gedacht habe war: ' + str(secretnumber))
